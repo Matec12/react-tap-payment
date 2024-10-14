7c8146a0ecd1f879061fa65d83f2bc31d15fd6e5
Alayo Ibrahim <34569440+Matec12@users.noreply.github.com>
2024-10-14 20:51:44 +0100

    feat: package done and ready to use

---
+++ example/src/App.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import { useTAPPaymentPayment, TAPPaymentButton } from './dist/index.es';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+const config = {
+  transID: `KLMNOYZabcdefghijkl${Math.random().toString(36).slice(2)}qrstuvwxyz`,
+  email: 'user@example.com',
+  amount: 20,
+  apiKey: 'TkdLb2VUMk46ZXRoWEdJSEF0Z24xOnB1WVUzd3dvS1c4bw==',
+  firstname: 'cool',
+  lastname: 'story'
+};
+
+const onSuccess = (response) => {
+  // Implementation for whatever you want to do with response and after success call.
+  console.log('response', response);
+};
+
+const onClose = () => {
+  // implementation for whatever you want to do when the TAPPayment dialog closed.
+  console.log('closed');
+};
+
+const TAPPaymentHookExample = () => {
+  const initializePayment = useTAPPaymentPayment(config);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          console.log('hell');
+          initializePayment({ onSuccess, onClose });
+        }}
+      >
+        TAPPayment Hooks Implementation
+      </button>
+    </div>
+  );
+};
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const componentProps = {
+    ...config,
+    text: 'TAPPayment Button Implementation',
+    onSuccess,
+    onClose
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <p>Library Samples</p>
+      </div>
+      <p className="read-the-docs">Click on the buttons to test either usecase</p>
+      <TAPPaymentHookExample />
+      <TAPPaymentButton {...componentProps} />
+    </div>
+  );
+}
+
+export default App;
